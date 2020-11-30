0e45afb292c105ebfd39f70c4a419a46c7817193
Martin M <martin.mosesson@gmail.com>
2020-11-30 22:02:28 +0100

    started working on mobile sizing

--- src/componnents/footer/Footer.js
+++ src/componnents/footer/Footer.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./Footer.css";
+import FacebookIcon from "../../assets/icons/facebook-square-brands.svg";
+import LinkedInIcon from "../../assets/icons/linkedin-brands.svg";
+import GithubIcon from "../../assets/icons/github-square-brands.svg";
+
+const FooterLink = (props) => {
+  return (
+    <div className='footer-link'>
+      <div className='icon-container--footer-link'>
+        <img src={props.icon} alt='footer-icon'></img>
+      </div>
+      <div className='title-container--footer-link'>
+        <h5>{props.children}</h5>
+      </div>
+    </div>
+  );
+};
 
 const Footer = (props) => {
-  return <footer className='footer'></footer>;
+  return (
+    <footer className='footer'>
+      <div className='footer-content-container'>
+        <h2>Interested in Collaboration?</h2>
+        <div className='text-button-container'>
+          <h5>
+            Challenge me. I am open to any idea - big or small. Feel free to
+            contact me here, or on my other social media platforms
+          </h5>
+          <button className='footer-button'>Let's connect</button>
+        </div>
+        <div className='footer-links-container'>
+          <FooterLink icon={LinkedInIcon}>LinkedIn</FooterLink>
+          <FooterLink icon={GithubIcon}>Github</FooterLink>
+          <FooterLink icon={FacebookIcon}>Facebook</FooterLink>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/sections/landing-section/Landing_section.js
+++ src/sections/landing-section/Landing_section.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import "./Landing_section.css";
 import WorkingMan from "../../assets/images/working-man.svg";
 import Background from "../../assets/images/lp-bg.svg";
 
 const LandingSection = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 760px)" });
+
   return (
     <div className='landing_section'>
       <div className='background_container'>
         <img src={Background} className='background__pattern'></img>
         <img src={WorkingMan} className='background__working_man'></img>
-      </div>
-      <div className='text_container'>
-        <h1 className='title--landing_section'>Hi, I am Martin Mosesson</h1>
-        <h3 className='subtitle--landing_section'>
-          A passionate Software Enginner from Stockholm
-        </h3>
-        <button className='button--landing_section'>LET'S TALK!</button>
+        <div className='text_container'>
+          <h1 className='title--landing_section'>Hi, I am Martin Mosesson</h1>
+          <h3 className='subtitle--landing_section'>
+            A passionate Software Enginner from Stockholm
+          </h3>
+          <button className='button--landing_section'>LET'S TALK!</button>
+        </div>
       </div>
     </div>
   );
